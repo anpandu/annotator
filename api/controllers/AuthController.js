e481e19876f0abe33667522f8c00ad3ed91354f5
anpandu <anpandumail@gmail.com>
2015-09-22 14:37:06 +0700

    Authentication: version 1.0

---
+++ api/controllers/AuthController.js
@@ -0,0 +1,53 @@
+/**
+* AuthController
+*
+* @description :: Authentication Controller
+* @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+*/
+
+var passport = require('passport');
+
+module.exports = {
+
+  _config: {
+    actions: false,
+    shortcuts: false,
+    rest: false
+  },
+
+  login: function(req, res) {
+
+    passport.authenticate('local', function(err, user, info) {
+      if ((err) || (!user)) {
+        return res.send({
+          message: info.message,
+          user: user
+        });
+      }
+      req.logIn(user, function(err) {
+        if (err) 
+          res.send(err);
+        res.redirect('/');
+        // return res.send({
+        //   message: info.message,
+        //   user: user
+        // });
+      });
+
+    })(req, res);
+  },
+
+  logout: function(req, res) {
+    req.logout();
+    res.redirect('/');
+  },
+
+  register: function(req, res) {
+    res.view('auth/register');
+  },
+
+  loginPage: function(req, res) {
+    res.view('auth/login');
+  }
+
+};

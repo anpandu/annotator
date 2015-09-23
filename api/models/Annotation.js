2332e8cc36e39cf7421a07c5e99bad08a6e35b44
anpandu <anpandumail@gmail.com>
2015-09-23 14:29:06 +0700

    annotation model + test

---
+++ api/models/Annotation.js
@@ -0,0 +1,18 @@
+/**
+* Annotation.js
+*
+* @description :: TODO: You might write a short summary of how this model works and what it represents here.
+* @docs        :: http://sailsjs.org/#!documentation/models
+*/
+
+module.exports = {
+
+  attributes: {
+
+    text: {type: 'string'},
+    value: {type: 'json'},
+    user : { model: 'user' }
+
+  }
+  
+};
